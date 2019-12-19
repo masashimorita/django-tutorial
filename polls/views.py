eac7c87cecae6b0b3b176a7d48c42aa331cab9fa
Masashi Morita <masashi.morita@opentone.co.jp>
2019-12-19 10:35:39 +0900

    :art: Use generic view class for writing less code

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import  loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import  generic
 from .models import Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", { 'question': question })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def result(request, question_id):
-    response = "You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choise = question.choise_set.get(pk=request.POST["choise"])
+    except (KeyError, Choise.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choise.votes += 1
+        selected_choise.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
